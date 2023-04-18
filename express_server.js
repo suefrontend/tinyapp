2b94a8ffc4dbcf192e7d846f9ad20cfa65f197db
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-18 14:02:42 -0700

    Add update function

--- express_server.js
+++ express_server.js
@@ -23,20 +23,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  console.log("req", req);
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//*******************
+// Display Home Page
+//*******************
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+//*******************
+// Display URL List
+//*******************
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  console.log("req.body.longURL", req.body.longURL);
   res.render("urls_index", templateVars);
 });
+
+//*******************
+// Add New URL
+//*******************
 
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
@@ -48,20 +58,9 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id],
-  };
-  console.log("templateVars", templateVars);
-  res.render("urls_show", templateVars);
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-
-  res.redirect("/urls");
-});
+//*******************
+// Display Sinle URL
+//*******************
 
 app.get("/u/:id", (req, res) => {
   const templateVars = {
@@ -72,6 +71,34 @@ app.get("/u/:id", (req, res) => {
     res.send("The URL doesn't exist.");
   }
   res.render("urls_show", templateVars);
+});
+
+//*******************
+// Edit URL
+//*******************
+
+app.post("/urls/:id", (req, res) => {
+  // Update urlDatabase
+  urlDatabase[req.params.id] = req.body.updatedURL;
+  res.redirect("/urls");
+});
+
+//*******************
+// Delete URL
+//*******************
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+
+  res.redirect("/urls");
 });
 
 app.get("/hello", (req, res) => {
