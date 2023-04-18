b02347c637760d04c51914fc3934cb4b937100ba
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-17 17:13:21 -0700

    Add a function to generate random string

--- express_server.js
+++ express_server.js
@@ -5,6 +5,8 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = () => {};
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
